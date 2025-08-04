a4813130aa05cbede2ffc4fb87496a25228bdcd9
Sharmila Rapeti <142177981+sharmila1320@users.noreply.github.com>
2025-08-04 10:41:17 +0530

    Update Showcase.jsx

--- client/src/components/sections/Showcase.jsx
+++ client/src/components/sections/Showcase.jsx
@@ -151,8 +151,8 @@ const Showcase = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="text-4xl font-bold text-primary-light">
-                    {cars[activeIndex].price}
+                   <div className="text-2xl sm:text-3xl md:text-4xl font-bold text-primary-light text-right break-words max-w-[6rem] sm:max-w-none">
+                      {cars[activeIndex].price}
                   </div>
                 </div>
               </motion.div>
